44ac1ea38e12faa12863ba334a2afb76198de17c
xirmsi <mathieu.snoriguzzi@vd.ch>
2015-07-03 16:48:30 +0200

    Ajout d'une clé

--- src/app/properties/propertieskeys.controller.js
+++ src/app/properties/propertieskeys.controller.js
@@ -11,21 +11,30 @@ angular.module('app')
 
   vm.application = Editor.editedObject();
 
+  function gotoHome() {
+    $location.path('/');
+  }
+
   PropertiesKeys.all(vm.application.id)
 	.success(function(data) { vm.propertieskeys = data; })
 	.error(function() { vm.errorMessage = 'Impossible de récupérer la liste des clés de propriété'})
 
 	vm.delete = function (propertykey) {
 		if (confirm('Êtes-vous sûr de vouloir supprimer cette clé de propriété ?')) {
-      Propertykey.delete(propertykey)
+      PropertiesKeys.delete(propertykey)
 			.success(function(data) { /* $location ? */ })
 			.error(function() { vm.errorMessage = 'Impossible de supprimer la clé de propriété'})
 		}
 	};
 
   vm.add = function () {
-    var property = {id : null, key : vm.key, value : null};
-    vm.propertieskeys.properties.push(property);
+    var propertykey = {id : null, key : vm.key, value : null};
+    PropertiesKeys.add(vm.application.id, propertykey)
+      .success(function(data) { vm.propertieskeys.properties.push(property); })
+      .error(function() { vm.errorMessage = 'Impossible d\'ajouter la clé de propriété'})
   }
 
+    vm.return = function () {
+      gotoHome();
+    };
 });
