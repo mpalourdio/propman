f867204691c42bfd04937ca20949d167c8394483
xirmsi <mathieu.snoriguzzi@vd.ch>
2015-07-03 12:07:04 +0200

    Ajout de la variable ENVIRONMENTS

--- src/app/components/config/backend.js
+++ src/app/components/config/backend.js
@@ -1,8 +1,9 @@
 'use strict';
 
-angular.module('app').constant('BACKEND_URL', 
-{ 
+angular.module('app').constant('BACKEND_URL',
+{
 	'USERS' : 'http://babar.jesc.ch:23456/users',
-	'APPLICATIONS' : 'http://babar.jesc.ch:23456/applications' 
+	'APPLICATIONS' : 'http://babar.jesc.ch:23456/applications',
+  'ENVIRONMENTS' : 'http://babar.jesc.ch:23456/environments'
 }
 );
