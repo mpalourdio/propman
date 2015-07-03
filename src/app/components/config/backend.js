1c0d0557771242fe860fe9048c6cf38abba9a9c1
xirmsi <mathieu.snoriguzzi@vd.ch>
2015-07-03 15:02:10 +0200

    Fix

--- src/app/components/config/backend.js
+++ src/app/components/config/backend.js
@@ -4,7 +4,7 @@ angular.module('app').constant('BACKEND_URL',
 {
 	'USERS' : 'http://babar.jesc.ch:23456/users',
 	'APPLICATIONS' : 'http://babar.jesc.ch:23456/applications',
-  'ENVIRONMENTS' : 'http://babar.jesc.ch:23456/env'
+  'ENVIRONMENTS' : 'http://babar.jesc.ch:23456/env',
   'PROPERTIES' : 'http://babar.jesc.ch:23456/properties'
 }
 );
