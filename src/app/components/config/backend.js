c688cd6b05e542949fb9be92462f680745e5480b
xirmsi <mathieu.snoriguzzi@vd.ch>
2015-07-03 14:56:56 +0200

    Ajout de la constante PROPERTY

--- src/app/components/config/backend.js
+++ src/app/components/config/backend.js
@@ -5,5 +5,6 @@ angular.module('app').constant('BACKEND_URL',
 	'USERS' : 'http://babar.jesc.ch:23456/users',
 	'APPLICATIONS' : 'http://babar.jesc.ch:23456/applications',
   'ENVIRONMENTS' : 'http://babar.jesc.ch:23456/env'
+  'PROPERTIES' : 'http://babar.jesc.ch:23456/properties'
 }
 );
