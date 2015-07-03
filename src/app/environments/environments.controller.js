cc31982c64037c85952959c0b0d4081d7cb4551e
mugor2 <suiss83@gmail.com>
2015-07-03 16:02:50 +0200

    Gestion des pages d'environnement

--- src/app/environments/environments.controller.js
+++ src/app/environments/environments.controller.js
@@ -1,21 +1,37 @@
 'use strict';
 
 angular.module('app')
-.controller('EnvironmentsCtrl', function (Environments, AuthService, USER_ROLES) {
+  .controller('EnvironmentsCtrl', function (Environments, AuthService, USER_ROLES, Editor) {
     var vm = this;
 
     vm.USER_ROLES = USER_ROLES;
     vm.AuthService = AuthService;
 
     Environments.all()
-      .success(function(data) { vm.environments = data; })
-      .error(function() { vm.errorMessage = 'Impossible de récupérer la liste des environnements'})
+      .success(function (data) {
+        vm.environments = data;
+      })
+      .error(function () {
+        vm.errorMessage = 'Impossible de récupérer la liste des environnements'
+      });
 
-    vm.delete = function (user) {
+    vm.delete = function (env) {
       if (confirm('Êtes-vous sûr de vouloir supprimer cet environnement ?')) {
-        Environments.delete(environment)
-          .success(function(data) { /* $location ? */ })
-          .error(function() { vm.errorMessage = 'Impossible de supprimer l\'environnement'})
+        Environments.delete(env)
+          .success(function () {
+            var index = vm.environments.indexOf(env);
+            if (-1 != index) {
+              vm.environments.splice(index,1);
+            }
+          })
+          .error(function () {
+            vm.errorMessage = 'Impossible de supprimer l\'environnement'
+          })
       }
     };
-});
+
+    vm.edit = function (env) {
+      var path = 'editenv/' + env.id;
+      Editor.edit(env, path);
+    }
+  });
