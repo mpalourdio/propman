85a7fa406b3e262a143c2548cfbcb2576518cb6d
mpalourdio <mpalourdio@gmail.com>
2015-07-03 11:52:05 +0200

    Add edit and del links

--- src/app/applications/applications.controller.js
+++ src/app/applications/applications.controller.js
@@ -4,19 +4,21 @@ angular.module('app')
     .controller('ApplicationsCtrl', function (Users, AuthService, USER_ROLES) {
         var vm = this;
 
-        vm.USER_ROLES = USER_ROLES;
+        vm.USER_ROLES  = USER_ROLES;
         vm.AuthService = AuthService;
 
         vm.applications = Users.all();
         vm.applications = [{
+            id:          "1",
             name:        "gpecs",
             logo:        "http://www.sirrix.com/media/images/60452.png",
             description: "Application qui ne fait rien"
         },
             {
+                id:          "2",
                 name:        "test",
                 logo:        "http://www.sirrix.com/media/images/60452.png",
                 description: "Application qui ne fait"
-            },
+            }
         ]
     });
