e620c0454539d67910b29b14b00523cf22c9725a
xirmsi <mathieu.snoriguzzi@vd.ch>
2015-07-03 11:55:28 +0200

    Ajout de lignes de test

--- src/app/environments/environments.controller.js
+++ src/app/environments/environments.controller.js
@@ -4,5 +4,18 @@ angular.module('app')
 .controller('EnvironmentsCtrl', function (Environments, AuthService, USER_ROLES) {
 	var vm = this;
 
-	vm.environments = Environments.all();
+	//vm.environments = Environments.all();
+    vm.environments = [{
+      shortname:"Prod",
+      longname: "Production"
+    },
+    {
+      shortname:"Valid",
+      longname: "Validation"
+    },
+    {
+      shortname:"Int",
+      longname: "Intégration"
+    },
+    ]
 });
